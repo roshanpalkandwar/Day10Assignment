b7510929e11a29fc67d035e331b6b82e56249675
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-11 12:51:34 +0530

    [roshan]contact

---
+++ Day10assignment/ContactDatails.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10assignment
+{
+    internal class ContactDatails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string Email { get; set; }
+
+        public static List<ContactDatails> ContactDetails = new List<ContactDatails>();
+        public static void Contact()
+        {
+            ContactDatails person = new ContactDatails();
+
+            Console.Write("Enter FirstName: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter LastName: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Mobile Number: ");
+            person.MobileNumber = Console.ReadLine();
+
+            Console.Write("Enter Address : ");
+            person.Address = Console.ReadLine();
+
+            Console.Write("Enter State : ");
+            person.State = Console.ReadLine();
+
+            Console.Write("Enter ZipCode : ");
+            person.Zipcode = Console.ReadLine();
+
+            Console.Write("Enter Email : ");
+            person.Email = Console.ReadLine();
+
+            ContactDetails.Add(person);
+
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Mobile Number: " + person.MobileNumber);
+            Console.WriteLine("Address : " + person.Address);
+            Console.WriteLine("State : " + person.State);
+            Console.WriteLine("ZipCode : " + person.Zipcode);
+            Console.WriteLine("Email : " + person.Email);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day10assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day10assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter choice\n1.contact details");
+            int opt=Convert.ToInt32(Console.ReadLine());
+
+            switch(opt)
+            {
+                case 1: ContactDatails.Contact();
+                   
+
+                    break;
+            }
+        }
+    }
+}
