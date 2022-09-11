72983131890d42ed3f97f97b157b296c02bf0563
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-11 13:04:13 +0530

    [roshan]Add Contact dtails

--- Day10assignment/Program.cs
+++ Day10assignment/Program.cs
@@ -4,14 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter choice\n1.contact details");
+            Console.WriteLine("enter choice\n1.contact details\n2.add contacts dtails");
             int opt=Convert.ToInt32(Console.ReadLine());
 
             switch(opt)
             {
                 case 1: ContactDatails.Contact();
-                   
-
+                     break;
+                    case 2: AddContacts.AddPerson();
                     break;
             }
         }
